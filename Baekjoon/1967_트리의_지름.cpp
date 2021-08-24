b8ecee515506970f9cc7ece44ee3678b69f32362
Byeongju Park <badmuxi@gmail.com>
2021-08-24 20:21:06 +0900

    Update 1967_트리의_지름.cpp

--- Baekjoon/1967_트리의_지름.cpp
+++ Baekjoon/1967_트리의_지름.cpp
@@ -3,46 +3,108 @@
 #include <queue>
 using namespace std;
 
+// 노드의 정보를 저장하는 구조체
 typedef struct Node{
 	int to,w;
 }node;
 
-int n;
-int ans;
-vector<node> v[10001];
-queue<node> q;
-
-int max(int a, int b){return a > b ? a : b;}
+int n;							// 노드의 개수
+int bigidx=1;					// 루트로 부터 가장 가중치 큰노드
+int ans=0;						// 출력할 값 (최대 지름)
+vector<node> v[10010];			// 노드 저장
+queue<node> q;					// BFS를 하기 위한 큐
+bool visited[10010] = {false,};	// visited
 
 void input()
 {
 	cin >> n;
 
-	for (int i=0; i<n; i++)
+	for (int i=0; i<n-1; i++)
 	{
 		int a,b,c;
 		cin >> a >> b >> c;
 		v[a].push_back({b,c});
+		v[b].push_back({a,c});
+	}
+}
+
+void visitedClear()
+{
+	for (int i=0; i<10010; i++)
+	{
+		visited[i] = false;
 	}
 }
 
 void BFS()
 {
-	// 초기 푸쉬
+	// 루트를 기점으로 BFS
 	for (int i=0; i<v[1].size(); i++)
 	{
 		int currTo = v[1][i].to;
 		int currW = v[1][i].w;
 		q.push({currTo,currW});
 	}
+	visited[1] = true;
 
 	while (!q.empty())
 	{
-		int currTo = q.front().to;
+		int currA = q.front().to;
+		int currW = q.front().w;
+		q.pop();
+		
+		visited[currA] = true;
+
+		if (currW > ans){
+			ans = currW;
+			bigidx = currA;
+		}
+
+		for (int i=0; i<v[currA].size(); i++)
+		{
+			int nextA = v[currA][i].to;
+			int nextW = v[currA][i].w;
+
+			if (visited[nextA] == true)
+				continue;
+
+			q.push({nextA,currW+nextW});
+		}
+	}
+
+	visitedClear();
+
+	// 가장 가중치가 높은 노드(bigidx)를 기준으로 BFS
+	for (int i=0; i<v[bigidx].size(); i++)
+	{
+		int currTo = v[bigidx][i].to;
+		int currW = v[bigidx][i].w;
+		q.push({currTo,currW});
+	}
+	visited[bigidx] = true;
+
+	while (!q.empty())
+	{
+		int currA = q.front().to;
 		int currW = q.front().w;
 		q.pop();
 
-		
+		visited[currA] = true;
+
+		if (currW > ans){
+			ans = currW;
+		}
+
+		for (int i=0; i<v[currA].size(); i++)
+		{
+			int nextA = v[currA][i].to;
+			int nextW = v[currA][i].w;
+
+			if (visited[nextA] == true)
+				continue;
+
+			q.push({nextA,currW+nextW});
+		}
 	}
 }
 
@@ -51,5 +113,8 @@ int main()
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-
+	input();
+	BFS();
+	cout << ans << '\n';
+	return 0;
 }
