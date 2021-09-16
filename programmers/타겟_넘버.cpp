5b525574d06f7e80c638bcdb980a0b611d3212fe
Byeongju Park <badmuxi@gmail.com>
2021-09-16 19:58:48 +0900

    Create 타겟_넘버.cpp

---
+++ programmers/타겟_넘버.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int ret = 0;
+
+void dfs(vector<int> numbers, int count, int curr, int target)
+{
+    if (count == numbers.size()) {
+        if (curr == target) ret++;
+        return;
+    }
+    
+    dfs(numbers,count+1,curr+numbers[count],target);
+    dfs(numbers,count+1,curr-numbers[count],target);
+}
+
+int solution(vector<int> numbers, int target) {
+    int answer = 0;
+    
+    dfs(numbers,0,0,target);
+    
+    answer = ret;
+    return answer;
+}
