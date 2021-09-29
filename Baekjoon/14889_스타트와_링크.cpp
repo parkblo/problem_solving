0bac6ca53a29f66d5033ccd34b0f80583812f707
Byeongju Park <badmuxi@gmail.com>
2021-09-29 19:47:43 +0900

    Update 14889_스타트와_링크.cpp

--- Baekjoon/14889_스타트와_링크.cpp
+++ Baekjoon/14889_스타트와_링크.cpp
@@ -1,5 +1,3 @@
-// 아직 풀고 있는 문제입니다.
-
 #include <iostream>
 #include <vector>
 #include <cmath>
@@ -11,65 +9,77 @@ int ar[21][21];
 int ret=1000000000;
 bool vis[21] = {false,};
 
+void printV(vector<int> start, vector<int> link)
+{
+	for (int i=0; i<start.size(); i++)
+		cout << start[i] << ' ';
+	cout << '\n';
+	for (int i=0; i<link.size(); i++)
+		cout << link[i] << ' ';
+	cout << '\n';
+}
+
 int func(vector<int> start, vector<int> link)
 {
 	int startValue = 0;
 	int linkValue = 0;
 
-	for (int i=1; i<=N/2; i++){
-		for (int j=i+1; j<=N/2; j++){
-			startValue = startValue + ar[i][j] + ar[j][i];
+	for (int i=0; i<start.size(); i++){
+		for (int j=0; j<start.size(); j++){
+			if (i==j) continue;
+			startValue += ar[start[i]][start[j]];
 		}
 	}
 
-	for (int i=1; i<=N/2; i++){
-		for (int j=i+1; j<=N/2; j++){
-			linkValue = linkValue + ar[i][j] + ar[j][i];
+	for (int i=0; i<link.size(); i++){
+		for (int j=0; j<link.size(); j++){
+			if (i==j) continue;
+			linkValue += ar[link[i]][link[j]];
 		}
 	}
 
 	return abs(startValue-linkValue);
 }
 
-void solve(vector<int> start, int cnt)
+void solve(int idx, int cnt)
 {
 	if (cnt == N/2)
 	{
+		vector<int> start;
 		vector<int> link;
 		for (int i=1; i<=N; i++)
 		{
-			if (vis[i] == false)
-				link.push_back(i);
+			if (vis[i]) start.push_back(i);
+			else link.push_back(i);
 		}
 		int value = func(start,link);
-		if (ret > value){
-			cout << value << '\n';
+		if (ret > value)
 			ret = value;
-		}
 		return;
 	}
 
-	for (int i=1; i<=N; i++)
+	for (int i=idx; i<=N; i++)
 	{
 		if (vis[i] == true) continue;
 
 		vis[i] = true;
-		start.push_back(i);
-		solve(start,cnt+1);
+		solve(i+1, cnt+1);
 		vis[i] = false;
 	}
 }
 
 int main()
 {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
 	cin >> N;
 
 	for (int i=1; i<=N; i++)
 		for (int j=1; j<=N; j++)
 			cin >> ar[i][j];
 	
-	vector<int> start;
-	solve(start,0);
+	solve(1,0);
 
 	cout << ret << '\n';
 }
