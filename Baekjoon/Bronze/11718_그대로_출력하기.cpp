a964cad76b697af4343b148765d5cd18deefce17
Byeongju Park <badmuxi@gmail.com>
2022-06-19 23:58:46 +0900

    Create 11718_그대로_출력하기.cpp

---
+++ Baekjoon/Bronze/11718_그대로_출력하기.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main()
+{ 
+    string str;
+    while (1){
+        getline(cin, str);
+        if (str=="")
+            break;
+        cout << str << endl;
+    } 
+    return 0;
+}
